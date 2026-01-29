b95b806628cc0a35ed4efa9032a19b9afa0e2d9f
Federica Lombardo <federica.lombardo121@gmail.com>
2026-01-29 21:09:36 +0000

    completed draft sgp4 propagator and mins between target and tle epoch

--- src/domain/TimeUtils.h
+++ src/domain/TimeUtils.h
@@ -3,10 +3,115 @@
 
 // ===============================
 
-// will build an inline function so that we eliminate the (small) overhead by calculating everything in a single step.
-// we are assuming we are dealing with times in the same day
+// will build an INLINE FUNCTION so that we eliminate the (small) overhead by calculating everything in a single step.
+
+// Assuming we are dealing with times in the same day
 inline double secondsBetween(const TimeUTC& t0, const TimeUTC& t1) {
     return (t1.hour - t0.hour) * 3600.0 +
             (t1.minute - t0.minute) * 60.0 +
             (t1.second - t0.second);
+}
+
+// Minutes from epoch to MJD2000 (decimal point)
+// TLE will come as (year, dayOfYear)
+// targetEpoch will come as (year, month, day, hour, minute, second)
+inline double epoch2MJD2000(const TimeUTC& targetTime) {
+
+    int year = targetTime.year;
+    int month = targetTime.month;
+    int day = targetTime.day;
+    int hour = targetTime.hour;
+    int minute = targetTime.minute;
+    int second = targetTime.second;
+
+    // Calculating amount of leap years between target year since 2000
+    int leapCount = 0;
+    for (int y = 2000; y < year; y++) {
+        if (leapYear(y)) {
+            leapCount++;
+        }
+    }
+ 
+    int febD;
+    if (leapYear(year)) {
+        int febD = 29;
+    } else {
+        int febD = 28;
+    }
+
+    int d;
+    switch (month-1) {
+    case 1:
+        int d = 31;
+        break;
+    case 2:
+        int d = 31 + febD;
+        break;
+    case 3:
+        int d = 31 + febD + 31;
+        break;
+    case 4:
+        int d = 31 + febD + 31 + 30;
+        break;
+    case 5:
+        int d = 31 + febD + 31 + 30 + 31;
+        break;
+    case 6:
+        int d = 31 + febD + 31 + 30 + 31 + 30;
+        break;
+    case 7:
+        int d = 31 + febD + 31 + 30 + 31 + 30 + 31;
+        break;
+    case 8:
+        int d = 31 + febD + 31 + 30 + 31 + 30 + 31 + 31;
+        break;
+    case 9:
+        int d = 31 + febD + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+        break;
+    case 10:
+        int d = 31 + febD + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+        break;
+    case 11:
+        int d = 31 + febD + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+        break;
+    case 12:
+        int d = 31 + febD + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31;
+        break;
+    default:
+        break;
+    }
+
+    int yearsSince = (365 * (year - 1 - 2000))*24*60; // [min]
+    int daysSince = (d + day - 1)*24*60;
+    int hoursSince = hour*60;
+    double secondsSince = second/60;
+    
+    double minuteSince = yearsSince + daysSince + hoursSince + minute + secondsSince;
+
+    return minuteSince;
+}
+
+inline double epoch2MJD2000_TLE(int year, double dayFrac) {
+    // Calculating amount of leap years between target year since 2000
+    int leapCount = 0;
+    for (int y = 2000; y < year; y++) {
+        if (leapYear(y)) {
+            leapCount++;
+        }
+    }
+
+    int yearsSince = (365 * (year - 1 - 2000))*24*60; // [min]
+    double dayMinSince = dayFrac*24*60;
+
+    double minuteSince = yearsSince + dayMinSince;
+
+    return minuteSince;
+}
+
+inline bool leapYear(int year) {
+    bool isLeap;
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        isLeap = 1; 
+    }
+    return isLeap;
 }
---
+++ src/propagation/Sgp4Propagator.cpp
@@ -0,0 +1,66 @@
+#include "Sgp4Propagator.h"
+extern "C" {
+    #include "../../external/sgp4/sgp4.h"
+}
+#include "../domain/TimeUtils.h"
+#include <cstdlib>
+#include <cmath>
+
+// ===============================
+
+// from this function we'll get a orbit state object with position and velocity vector plus time, using the function propagate() of the object SGP4Propagator
+OrbitState Sgp4Propagator::propagate(
+    const Tle& tle,
+    const TimeUTC& targetTime
+) const {
+    // using SGP4 library internal satellite record
+    elsetrec satrec{};   
+
+    // defining extra arguments:
+    double typerun = 'c'; // catalog run
+    double typeinput = 'i'; // input TLE
+    char opsmode = 'a'; // AFSPC Mode, check what it is
+    gravconsttype whichconst = wgs72; // or WGS84, depending on your repo
+    double startmfe = 0.0;
+    double stopmfe = 0.0;
+    double deltamin = 0.0;
+
+
+    // parse TLE lines into satrec
+    SGP4Funcs::twoline2rv(
+        const_cast<char*>(tle.line1.c_str()),
+        const_cast<char*>(tle.line2.c_str()),
+        typerun,
+        typeinput,
+        opsmode,
+        whichconst,
+        startmfe,
+        stopmfe,
+        deltamin,
+        satrec
+    );
+
+    // Computing mins since MJD2000 epoch from TLE epoch
+    int yearTLE = std::stoi(tle.line1.substr(19,20));
+    int dayOfYearTLE = std::stoi(tle.line1.substr(21,32));
+
+    double minutesSinceEpochTLE = epoch2MJD2000_TLE(yearTLE, dayOfYearTLE);
+
+    // Computing mins since MJD2000 epoch from targetTime
+    double minutesSinceEpochTarget = epoch2MJD2000(targetTime);
+
+    // Difference target time and tle epoch time
+    double m = minutesSinceEpochTarget - minutesSinceEpochTLE;
+
+    // Propagate orbit using SGP4
+    double r[3], v[3]; // TEME reference frame in [km] and [km/s]
+    SGP4Funcs::sgp4(satrec, m, r, v);
+    
+    return {
+        {r[0], r[1], r[2]},
+        {v[0], v[1], v[2]},
+        targetTime
+    };
+}
+
+// epoch2MJD2000(int targetYear, double dayFraction)
