961744ce193e3a18c4b04567e16d7943d967ce27
Federica Lombardo <federica.lombardo121@gmail.com>
2026-05-20 12:07:11 +0100

    cleaned main.cpp and created projecet frontend

---
+++ frontend/SatelliteTracker/SatelliteTracker/SatelliteTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatelliteTrackerApp.swift
+//  SatelliteTracker
+//
+//  Created by Federica Lombardo on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SatelliteTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
