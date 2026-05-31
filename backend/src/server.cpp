828b2c1fddeea0552e348f25d639e23e69d5af2f
Federica Lombardo <federica.lombardo121@gmail.com>
2026-05-31 10:52:41 +0100

    completed groundtrack/name endpoint fetching tle from celestrak and parsing the result

--- backend/src/server.cpp
+++ backend/src/server.cpp
@@ -75,38 +75,64 @@ int main() {
     });
 
     svr.Post("/groundtrack/name", [](const httplib::Request& req, httplib::Response& res) {
-        std::cout << "method called";
         auto body = nlohmann::json::parse(req.body);
-        std::cout << body;
 
         // Input type: Satellite Name (already parsed with spaces removed)
         std::string satName = body["name"].get<std::string>();
-        std::cout << satName;
 
         // Celestrak URL using NAME as research
         std::string urlName = "/NORAD/elements/gp.php?NAME=" + satName + "&FORMAT=tle";
 
         // Fetch TLE from Celestrak client
-        httplib::Client cli("https://celestrak.org");
+        httplib::Client cli("http://celestrak.org");
         auto cliRes = cli.Get(urlName);
-        auto tleJSON = nlohmann::json::parse(cliRes->body);
+        auto rawTle = cliRes->body;
+        std::cout << "RAW TLE: [" << rawTle << "]" << std::endl;
 
+        // Parsing TLE from celestrak
         Tle tle;
-        tle.name = body["name"].get<std::string>();
-        tle.line1 = body["line1"].get<std::string>();
-        tle.line2 = body["line2"].get<std::string>();
+        std::istringstream stream(rawTle);
+        std::getline(stream, tle.name);
+        std::getline(stream, tle.line1);
+        std::getline(stream, tle.line2);
+        tle.name.erase(tle.name.find_last_not_of(" \t\r\n") + 1);
+        tle.line1.erase(tle.line1.find_last_not_of(" \t\r\n") + 1);
+        tle.line2.erase(tle.line2.find_last_not_of(" \t\r\n") + 1);
 
         std::cout << tle.line1;
 
-        // For now let's harcode input timeS, timeE. Then will be added to the body of the request.
-        TimeUTC timeS = {2026, 2, 16, 1, 0, 0.0};
-        validate(timeS);
-        TimeUTC timeE = {2026, 2, 16, 5, 30, 0.0};
-        validate(timeE);
+        // Fetch Start and End timeUTC (from TLE body - JSON)
+        std::string startString = body["startTime"].get<std::string>();
+        std::string endString = body["endTime"].get<std::string>();
 
-        // Use function propagate() to get the grountrack object
-        double step = 60.0 / 60.0; // [min] considering now 1 minute at a time
-        std::vector<GroundTrack> gt = propagate(tle, timeS, timeE, step);
+        // Initialise the TimeUTC object and fetch components
+        TimeUTC start;
+        TimeUTC end;
+        // ------------------
+        start.year = std::stoi(startString.substr(0,4));
+        start.month = std::stoi(startString.substr(5,2));
+        start.day = std::stoi(startString.substr(8,2));
+        start.hour = std::stoi(startString.substr(11,2));
+        start.minute = std::stoi(startString.substr(14,2));
+        start.second = std::stod(startString.substr(17,2));
+        // ------------------
+        end.year = std::stoi(endString.substr(0,4));
+        end.month = std::stoi(endString.substr(5,2));
+        end.day = std::stoi(endString.substr(8,2));
+        end.hour = std::stoi(endString.substr(11,2));
+        end.minute = std::stoi(endString.substr(14,2));
+        end.second = std::stod(endString.substr(17,2));
+
+        // Get the step from the user (if any)
+        std::string stepString = body["stepInterval"].get<std::string>();
+        double step;
+        if (stepString != "") {
+            step = std::stod(stepString);
+        } else {
+            step = 60.0 / 60.0; // default value: 1 min [min]
+        }
+
+        std::vector<GroundTrack> gt = propagate(tle, start, end, step);
         std::vector<double> lat_vec, lon_vec;
         for (int i = 0; i < gt.size(); i++) {
             lat_vec.push_back(gt[i].lat);
@@ -124,6 +150,7 @@ int main() {
         }
 
         res.set_content(result.dump(), "application/json");
+        
 
     });
 
